4cbd42f27ccd6ec58ff5da88d886039d387f8560
Everaldo Canuto <everaldo.canuto@gmail.com>
2020-08-15 14:14:42 -0300

    Improve store tests

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -31,3 +31,56 @@ func TestStoreDefaultDriver(t *testing.T) {
 	}
 	defer store.Close()
 }
+
+func TestStoreAuthEmpty(t *testing.T) {
+	store, err := Default()
+	if err != nil {
+		t.Errorf("Error \"%v\".", err)
+	}
+	defer store.Close()
+
+	expect := false
+	result := store.Auth("")
+	if result != expect {
+		t.Errorf("Expected %v and got %v.", expect, result)
+	}
+}
+
+func TestStoreAuth(t *testing.T) {
+	store, err := Default()
+	if err != nil {
+		t.Errorf("Error \"%v\".", err)
+	}
+	defer store.Close()
+
+	token := "qwert"
+	store.AuthToken = token
+	expect := true
+	result := store.Auth(token)
+	if result != expect {
+		t.Errorf("Expected %v and got %v.", expect, result)
+	}
+}
+
+func TestStorePutGet(t *testing.T) {
+	store, err := Default()
+	if err != nil {
+		t.Errorf("Error \"%v\".", err)
+	}
+	defer store.Close()
+
+	url := "https://github.com/noverde/pequi"
+	res, err := store.Put(url)
+	if err != nil {
+		t.Errorf("Error \"%v\".", err)
+	}
+
+	val, err := store.Get(res)
+	if err != nil {
+		t.Errorf("Error \"%v\".", err)
+	}
+	if val != url {
+		t.Errorf("Expected \"%v\" and got \"%v\".", val, url)
+	}
+
+}
